f813b4416300178480725c656537b0adba32e64e
diegotreto1 <diegotreto1087@gmail.com>
2021-09-02 09:28:28 -0700

    hello

--- Unit1/fonts/sketch.js
+++ Unit1/fonts/sketch.js
@@ -3,15 +3,18 @@ var f1 ;
 function setup() {
   createCanvas(500, 500);
   f1 = loadFont("assets/Nowharehouse.ttf") ;
+  f2 = loadFont("assets/font1.TTF") ;
+  textAlign(CENTER) ;
 }
 
 function draw() {
 background('yellow')
   textFont(f1) ;
   textSize(48) ;
-  text('hello world', 100, 100) ;
-  textSize(28) ;
-  text('hello world', 50, 50) ;
+  text('hello world', width/2, 100) ;
 
+textFont(f2);
+  textSize(28) ;
+  text('whats up', width/2, 160) ;
 
 }
