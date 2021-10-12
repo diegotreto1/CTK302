3880b5f46c5bb011472479f79c085736751399cf
diegotreto1 <diegotreto1087@gmail.com>
2021-10-11 18:57:35 -0700

    update

--- Unit2/touchin/sketch.js
+++ Unit2/touchin/sketch.js
@@ -4,12 +4,19 @@ let numberOfTouches;
 
 function setup() {
   createCanvas(400, 400);
+
+
+  img1 = loadImage("assets/desync.png");
+  img1 = loadImage("assets/feedback.png");
+  img1 = loadImage("assets/velvetdesync.png");
+  imageMode(CENTER);
 }
 
 function draw() {
   clear();
   numberOfTouches = touches.length;
-  text(numberOfTouches + ' touches', 5, 10);
+  text(numberOfTouches + 'touches', 5, 10);
+  background('green');
 
   switch (numberOfTouches) {
     case 0:
@@ -19,21 +26,21 @@ function draw() {
     case 1:
       text("i think my tv broke", 5, 22);
       // put a picture here
-      img1 = loadImage("assets/desync.png");
+      image(img1, width / 2, height / 2, 100, 100);
       break;
 
     case 2:
       text("can you fix it?", 5, 22);
       // put a picture here
-      img1 = loadImage("assets/velvetdesync.png");
+      image(img2, width / 2, height / 2 - 120, 100, 100);
       break;
 
     case 3:
       text("thankyou!", 5, 22);
       // put a picture here
-      img1 = loadImage("assets/feedback.png");
+      image(img3, width / 2, height / 2 + 120, 100, 100);
       break;
 
 
-}
+  }
 }
